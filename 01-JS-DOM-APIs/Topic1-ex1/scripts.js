b355d98c51a3e5c9c1d47155ed3f0c9bef9bf389
EzequielR <ezequielrebasa@gmail.com>
2020-03-06 10:52:53 -0300

    topic1 ex-1

---
+++ 01-JS-DOM-APIs/Topic1-ex1/scripts.js
@@ -0,0 +1,8 @@
+const section = document.querySelector('.section-hidden-content');
+
+window.onload = () => section.style.opacity = 1;
+
+/*window.onload = function () {
+     section.style.opacity = 1;
+}*/
+
