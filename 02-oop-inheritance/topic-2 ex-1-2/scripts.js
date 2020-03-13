e4845c10fb607ef65fa0dded1355fb051cc78e26
EzequielR <ezequielrebasa@gmail.com>
2020-03-13 18:40:42 -0300

    topic-2 ex-4

--- 02-oop-inheritance/topic-2 ex-1-2/scripts.js
+++ 02-oop-inheritance/topic-2 ex-1-2/scripts.js
@@ -117,9 +117,9 @@ lor.off('pause', callbackPause);
 const actor1 = new Actor("Rayo McQueen", 20);
 const actor2 = new Actor("Turbo Lopez", 23);
 const actors = [new Actor("Arthur Ito", 45),
-                new Actor("Esteban Quito", 37),
-                new Actor("Facundo Pastor", 36)
-                ];
+new Actor("Esteban Quito", 37),
+new Actor("Facundo Pastor", 36)
+];
 
 cars.addActor(actor1);
 cars.addActor(actor2);
@@ -129,10 +129,28 @@ cars.addActor(actors);
 
 const logger = new Logger();
 
-cars.on('play',logger.log);
-cars.on('pause',logger.log); 
-cars.on('resume', logger.log); 
+cars.on('play', logger.log);
+cars.on('pause', logger.log);
+cars.on('resume', logger.log);
 
 cars.play(); // output : The play event has been emmited
 cars.pause(); // output : The pause event has been emmited
 cars.resume(); // output : The pause event has been emmited
+
+
+///////////////////////////////////////// Exercise 4 ////////////////////////////////////
+
+const social = {
+    like(friendName){
+        console.log(friendName +` likes ${this.title} `);
+    },
+    share(friendName){
+        console.log( friendName + ` share ${this.title} `);
+    }
+};
+
+// Mixin using the prototype
+
+Object.assign(Movie.prototype, social);
+
+cars.like("Marcos")
